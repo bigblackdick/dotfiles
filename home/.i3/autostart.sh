50d15d539d08d937b6ec640a37c87c4d37525702
bbd <bbd@example.com>
2016-10-25 12:47:28 +0300

    Switched to py3status.

---
+++ home/.i3/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+/usr/bin/nitrogen --restore
+
+#/usr/bin/i3-msg "workspace M; exec chromium-browser --user-data-dir=/tmp/$RANDOM --no-proxy-server --start-maximized --app=file:///home/test/tmp/mon.html;split horizontal; exec urxvt -e newsbeuter;"
+
+#while [ 1 ];
+#do
+#    if [ `xprintidle` -gt 120000 ];
+#    then
+#    /usr/bin/i3-msg  "workspace M"
+#    fi
+#    sleep 10
+#done 
